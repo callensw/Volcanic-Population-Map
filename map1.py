328e26eea3e748b9552ef067d25b1aa68b2eeb75
Chase <allensworthchase@gmail.com>
2021-09-24 10:57:01 -0500

    Final

--- map1.py
+++ map1.py
@@ -22,16 +22,24 @@ Height: %s m
 """
 
 map = folium.Map(location = [38.58, -99.09], zoom_start = 5, tiles = "Stamen Terrain")
-fg = folium.FeatureGroup(name = "My Map")
+
+fgv = folium.FeatureGroup(name = "My Map")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = folium.Popup(iframe),
+    fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = folium.Popup(iframe),
     fill_color = color_producer(el), color = 'grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(),
+fgp = folium.FeatureGroup(name = "Population")
+
+fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(),
 style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
 map.save("Map1.html")
