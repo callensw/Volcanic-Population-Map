085295c8c98a5428701ac998576c48c74cab6ad8
Chase <allensworthchase@gmail.com>
2022-05-31 10:52:41 -0500

    update 5.31.22

--- map1.py
+++ map1.py
@@ -1,12 +1,15 @@
+]# Import required libraries
 import folium
 import pandas as pd
 
+# Creating required variables
 data = pd.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
 
+# Function for Elevation color determination
 def color_producer(elevation):
     if elevation < 1000:
         return 'green'
@@ -20,26 +23,27 @@ Volcano name:<br>
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
 Height: %s m
 """
-
+# Generating map with foliun
 map = folium.Map(location = [38.58, -99.09], zoom_start = 5, tiles = "Stamen Terrain")
 
-fgv = folium.FeatureGroup(name = "My Map")
-
-for lt, ln, el, name in zip(lat, lon, elev, name):
-    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = folium.Popup(iframe),
-    fill_color = color_producer(el), color = 'grey', fill_opacity=0.7))
-
+# Adding population layer to map
 fgp = folium.FeatureGroup(name = "Population")
 
 fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(),
 style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
+map.add_child(fgp)
 
+# Adding volcano description  layer to map
+fgv = folium.FeatureGroup(name = "My Map")
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = folium.Popup(iframe),
+    fill_color = color_producer(el), color = 'grey', fill_opacity=0.7))
 
 map.add_child(fgv)
-map.add_child(fgp)
+
 map.add_child(folium.LayerControl())
 
 map.save("index.html")
